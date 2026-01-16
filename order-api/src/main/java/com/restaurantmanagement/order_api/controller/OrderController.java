adf6a35da91753660a2e7d8ccb1ed0b4ab5c652f
FrostScythe <maheshwariayush371@gmail.com>
2026-01-16 15:06:54 +0530

    Completed exception handling

--- order-api/src/main/java/com/restaurantmanagement/order_api/controller/OrderController.java
+++ order-api/src/main/java/com/restaurantmanagement/order_api/controller/OrderController.java
@@ -4,7 +4,6 @@ import com.restaurantmanagement.order_api.entity.Order;
 import com.restaurantmanagement.order_api.entity.OrderStatus;
 import com.restaurantmanagement.order_api.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
